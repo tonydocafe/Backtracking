00c3a5197988af4e1cf19ef862a759b05b7d0ee9
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-04-17 17:54:08 -0300

    Update nqueens.cpp

--- C++/nqueens.cpp
+++ C++/nqueens.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <vector>
+
+#define N 8  
+
+using namespace std;
+
+
+void solution(const vector<vector<int>>& table) {
+    for (const auto& row : table) {
+        for (int cell : row) {
+            cout << (cell == 1 ? "Q" : ".") << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
 
