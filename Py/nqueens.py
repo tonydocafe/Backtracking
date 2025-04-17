4048285426dc7f6bc81c6d43a40730983a3f490d
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-04-16 21:53:08 -0300

    Update nqueens.py

--- Py/nqueens.py
+++ Py/nqueens.py
@@ -4,3 +4,7 @@ def solution(table):
     for row in table:
         print(" ".join("Q" if cell else "." for cell in row))
     print()
+def checkposition(table, row, col):
+    for i in range(table):
+        if table[row][i]:
+            return False
